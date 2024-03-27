2a894d4e12aae0cf8d2097eb75429034f9f12206
Zawwar Khatri <159942012+Zawwar6@users.noreply.github.com>
2024-03-27 16:52:54 +0500

    added index.html

---
+++ app.js
@@ -0,0 +1,33 @@
+function clickMe(){
+    let randomNumber = Math.floor(Math.random()*1000) +1;
+    alert("Random number: " +randomNumber);
+}
+function clickMe2(){
+    let randomNumber = Math.floor(Math.random()*1000) +1;
+    alert("Random number: " +randomNumber);
+}
+function clickMe3(){
+    let randomNumber = Math.floor(Math.random()*1000) +1;
+    alert("Random number: " +randomNumber);
+}
+function clickMe4(){
+    let randomNumber = Math.floor(Math.random()*1000) +1;
+alert("Random number: " +randomNumber);
+}
+function clickMe5(){
+    let randomNumber = Math.floor(Math.random()*1000) +1;
+alert("Random number: " +randomNumber);
+}
+function clickMe6(){
+    let randomNumber = Math.floor(Math.random()*1000) +1;
+    alert("Random number: " +randomNumber);
+}
+function clickMe7(){
+    let randomNumber = Math.floor(Math.random()*1000) +1;
+    alert("Random number: " +randomNumber);
+}
+function clickMe8(){
+    let randomNumber = Math.floor(Math.random()*1000) +1;
+    alert("Random number: " +randomNumber);
+}
+
